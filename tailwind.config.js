5844f206fff0dcc605eed082f507cedb714bfb89
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2025-06-29 16:34:50 +0700

    fix: Add missing dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+import { Config } from 'tailwindcss';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+      },
+    },
   },
   plugins: [],
-};
+} satisfies Config;
