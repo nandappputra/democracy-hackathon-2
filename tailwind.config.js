61efdcb3f1d131250e06edaed4034ba0e024d4ae
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2025-06-29 16:03:38 +0700

    fix: Add missing files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',  // Scan all React component files
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  safelist: [
-    {
-      pattern: /(bg|text|from|to)-(blue|yellow|green|pink)-(100|600)/,
-    },
-  ],
   plugins: [],
 };
